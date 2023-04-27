d39d3b2b122a7be76196036c0afdb8d61e8e9594
Sermob112 <motherboard879@gmail.com>
2023-04-27 17:36:08 +0300

    Сделан скелет

--- main.py
+++ main.py
@@ -1,20 +1,10 @@
 
-
-
-from Crypto.Cipher import AES
-import numpy as np
-import cv2
-import barcode
-from barcode import Code128
-from PIL import Image
-from barcode.writer import ImageWriter
-
 import cv2
 import numpy as np
 import imageio
-import io
+
 from stegano import lsb
-from barcode import Code128
+from barcode import EAN8,Code128
 from barcode.writer import ImageWriter
 from PIL import Image
 from pyzbar.pyzbar import decode
@@ -34,7 +24,7 @@ def Hide_image():
     # Сохранение скрытого изображения
     encrypted_img.save('encrypted_image.png')
 
-def decoder():
+def decoder(img):
     img = Image.open("encrypted_image.png")
 
     # Извлечение скрытой информации из изображения
@@ -45,3 +35,20 @@ def decoder():
         print(i.data.decode("utf-8"))
 
 # decoder()
+def Haart_casc():
+    face_cascade = cv2.CascadeClassifier('cascade/haarcascade_frontalface_default.xml')
+    img_rgb = cv2.imread('5.png')
+    img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(img_gray, 1.3, 5)
+    data = str(faces)
+    ean8 = Code128(data, writer=ImageWriter())
+    barcode_img = ean8.save('barcode128')
+    decoded_data = decode(Image.open('barcode128.png'))
+    for i in decoded_data:
+        dec = i.data.decode("utf-8")
+
+    if str(faces) == dec:
+        print('Face verified!')
+    else:
+        print('Face not verified!')
+Haart_casc()
